eb4347f0f6de533aed0f5b3d7340a461d3cbc51f
Hasan Afridi <hasan.afridi@outlook.com>
2024-04-09 16:05:53 +0100

    add gameboard

---
+++ src/components/Player.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [player, setPlayer] = useState(name);
+
+  const handleEditClick = () => {
+    setIsEditing((editing) => !editing);
+  };
+
+  const handleChange = (e) => {
+    setPlayer(e.target.value);
+  };
+
+  let playerName = <span className="player-name">{player}</span>;
+
+  if (isEditing) {
+    playerName = (
+      <input
+        type="text"
+        required
+        value={player}
+        onChange={handleChange}
+      ></input>
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
