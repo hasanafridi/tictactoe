282de1b38cf8b226573eb7f1833a72982957f440
Hasan Afridi <hasan.afridi@outlook.com>
2024-10-31 09:26:51 +0000

    update current player

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ name, symbol, isActive }) {
   const [isEditing, setIsEditing] = useState(false);
   const [player, setPlayer] = useState(name);
 
@@ -26,7 +26,7 @@ export default function Player({ name, symbol }) {
   }
 
   return (
-    <li>
+    <li className={isActive ? "active" : ""}>
       <span className="player">
         {playerName}
         <span className="player-symbol">{symbol}</span>
